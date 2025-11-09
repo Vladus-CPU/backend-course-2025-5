6a812ee32dd7e2956df255608abeddf9ed9ccc7a
Качановський Владислав bc2025-5 <kacanovskijvlad@gmail.com>
2025-11-09 06:01:52 +0200

    added method GET, added in gitignore jpeg/jpg files, added testing information in test.txt

--- main.js
+++ main.js
@@ -16,9 +16,26 @@ if (!fs.existsSync(options.cache)) {
         recursive: true 
     });
 }
-const server = http.createServer(function (request, response){
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end();
+const server = http.createServer(async function (request, response){
+    const url = request.url;
+    const statuscode = url.replace('/', '');
+    if (statuscode.length !== 3  || isNaN(statuscode)) {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        return response.end('Not Found');
+    }
+    const cachefilepath = options.cache + '/' + statuscode + '.jpeg';
+    const method = request.method;
+    const promise = fs.promises;
+    if (method === 'GET') {
+        try {
+            const data = await promise.readFile(cachefilepath);
+            response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            return response.end(data);
+        } catch {
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            return response.end('Not Found');
+        }
+    }
 });
 
 server.listen(options.port, options.host, function () {
